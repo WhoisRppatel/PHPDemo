a9dd8d3664095f911e93c7255aa0f11d78383c16
Patel Rutvik P <rutspatel1999@gmail.com>
2020-03-15 14:06:47 +0530

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -3,7 +3,7 @@ $unm=$_POST["unm"];
 $pass=$_POST["pass"];
 if (($unm=="divyesh" || $unm=="rutvik") && ($pass=="0007" || $pass=="9006") )
 {
-  header('Location: home.html');
+  header('Location: https://www.youtube.com/channel/UCMgvAFjbSBZPVDAx2Hda3Dw/');
   exit;  
 }
 else{
