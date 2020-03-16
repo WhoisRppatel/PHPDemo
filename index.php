97f56042c059f60ad184ab386b2c492c2d30d97a
Patel Rutvik P <rutspatel1999@gmail.com>
2020-03-16 16:24:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,78 +1,58 @@
-<!DOCTYPE html>
 <html>
-    <head>
-        <title>Login</title>
-        <link rel="stylesheet" href="Style.css">
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 	<style>
-		body{
-    margin: 0;
-    padding: 0;
-    font-family: sans-serif;
-    background: #455A64;
-    background-image:  url(img/background.jpg);
-}
-.Box{
-    width: 300px;
-    padding: 40px;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%,-50%);
-    background: #37474F;
-    text-align: center;
-    opacity: 0.6;
-}
-.Box h1{
-    color: white;
-    text-transform: uppercase;
-    font-weight: 500;
-}
-.Box input[type = "text"],.Box input[ type = "Password"]{
-    border: 0;
-    background: none;
-    display: block;
-    margin: 20px auto;
-    text-align: center;
-    border: 2px solid #03A9F4;
-    padding: 14px 10px;
-    width: 200px;
-    outline: none;
-    color: white;
-    border-radius: 24px;
-    transition: 0.25s;
-}
-
-.Box input[type = "text"]:focus,.Box input[ type = "Password"]:focus{
-    width: 280px;
-    border-color: #8BC34A;
-}
-.Box input[Type="submit"]{
-    border: 0;
-    background: none;
-    display: block;
-    margin: 20px auto;
-    text-align: center;
-    border: 2px solid #8BC34A;
-    padding: 14px 40px;
-    outline: none;
-    color: white;
-    border-radius: 24px;
-    transition: 0.25s;
-    cursor: pointer;
-}
-.Box input[type="submit"]:hover{
-    background: #8BC34A;
-}
-
+		.modal-content{
+			background-color: darkcyan;
+		}
+		.btn-link{
+			color:white;
+		}
+		.modal-heading h2{
+			color:#ffffff;
+		}
 	</style>
-    </head>
-    <body>
-            <form class="Box" action="welcome.php" method="POST">
+	
+</head>
+<body>
+<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-heading">
+				<h2 class="text-center">Login</h2>
+			</div>
+			<hr />
+			<div class="modal-body">
+				<form action="" role="form">
+					<div class="form-group">
+						<div class="input-group">
+							<span class="input-group-addon">
+							<span class="glyphicon glyphicon-user"></span>
+							</span>
+							<input type="text" class="form-control" placeholder="User Name" />
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="input-group">
+							<span class="input-group-addon">
+							<span class="glyphicon glyphicon-lock"></span>
+							</span>
+							<input type="password" class="form-control" placeholder="Password" />
 
-                <h1>Login</h1>
-                <input name="unm" required type="text"placeholder="Username">
-                <input name="pass" required type="password"placeholder="Password">
-                <input type="submit"value="Login" >
-            </form>
-    </body>
-</html>
+						</div>
+
+					</div>
+
+					<div class="form-group text-center">
+						<button type="submit" class="btn btn-success btn-lg">Login</button>
+						<a href="#" class="btn btn-link">forget Password</a>
+					</div>
+
+				</form>
+			</div>
+		</div>
+	</div>
+	</body>
+	</html>
